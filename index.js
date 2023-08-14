ef94bc071a7a856ee8db3150628484cdf961ffc4
Kelvin Gil <gilkelvin2@gmail.com>
2023-08-14 12:58:56 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// write a functiion superbowlwins and hvae it return the years that the denver broncos have won a superbowl
+// and then have it return undefined if the record has no win objects 
+function callBack(eachRecord){
+    console.log(eachRecord)
+    let x = eachRecord.result;
+    if( x === "W" ){
+        return eachRecord;
+    }
+
+}
+
+function superbowlWin(record){
+   
+    const win = record.find(callBack);
+    if( win === undefined){
+        return undefined;
+    }
+    return win.year;
+}
+
+
+
